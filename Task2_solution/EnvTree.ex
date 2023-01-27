c13030e944e82a6db6d4ddeacdeba9c1a515f1b0
Adrian Jonsson Sjoedin <adrian.jonsson.sjoedin@gmail.com>
2023-01-27 08:05:44 +0100

    tree program done

--- Task2_solution/EnvTree.ex
+++ Task2_solution/EnvTree.ex
@@ -13,7 +13,7 @@ defmodule EnvTree do
   end
 
   # return a tree that looks like the one we have but where the left branch has been updated
-  def add({:node, k, v, left, right}, key, value) when k < key do
+  def add({:node, k, v, left, right}, key, value) when key < k do
     {:node, k, v, add(left, key, value), right}
   end
 
@@ -29,8 +29,8 @@ defmodule EnvTree do
     {key, value}
   end
 
-  # if the key we're searching for is larger than where we're at we go down left
-  def lookup({:node, k, _, left, _}, key) when k < key do
+  # if the key we're searching for is smaller than where we're at we go down left
+  def lookup({:node, k, _, left, _}, key) when key < k do
     lookup(left, key)
   end
 
@@ -44,4 +44,28 @@ defmodule EnvTree do
   def remove({:node, key, _, nil, right}, key), do: right
   #same but for the opposite
   def remove({:node, key, _, left, nil}, key), do: left
+  #if the key is found but it have branches down left and right, we have to replace it with
+  #the leftmost pair in the right branch
+  def remove({:node, key, _, left, right}, key) do
+    {key, value, rest} = leftmost(right) #pattern match with what leftmost returns
+    {:node, key, value, left, rest}
+  end
+  #when the key we want to remove is smaller than where we're at we go down left and look
+  def remove({:node, k, v, left, right}, key) when key < k do
+    {:node, k, v, remove(left, key), right}
+  end
+  #if it's larger than where we're at we go down right
+  def remove({:node, k, v, left, right}, key) do
+    {:node, k, v, left, remove(right, key)}
+  end
+
+  #if the right branch doesn't have a left branch we can simply return
+  def leftmost({:node, key, value, nil, rest}) do
+    {key, value, rest}
+  end
+  #otherwise we keep going recursively until we find one that has
+  def leftmost({:node, k, v, left, right}) do
+    {key, value, rest} = leftmost(left)
+    {key, value, {:node, k, v, rest, right}}
+  end
 end
