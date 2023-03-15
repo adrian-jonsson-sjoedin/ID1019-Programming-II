29f9031fee9ee05fa73456d64e9fd0bdc7917bf0
Adrian Jonsson Sjoedin <adrian.jonsson.sjoedin@gmail.com>
2023-03-15 14:39:58 +0100

    fixed the encoding. code is now done

--- Task9_Solution/morse.ex
+++ Task9_Solution/morse.ex
@@ -1,5 +1,48 @@
 defmodule Morse do
-  def morse_tree do
+  # Morse representation of the alphabet
+  @morse_codes %{
+    ?a => ".-",
+    ?b => "-...",
+    ?c => "-.-.",
+    ?d => "-..",
+    ?e => ".",
+    ?f => "..-.",
+    ?g => "--.",
+    ?h => "....",
+    ?i => "..",
+    ?j => ".---",
+    ?k => "-.-",
+    ?l => ".-..",
+    ?m => "--",
+    ?n => "-.",
+    ?o => "---",
+    ?p => ".--.",
+    ?q => "--.-",
+    ?r => ".-.",
+    ?s => "...",
+    ?t => "-",
+    ?u => "..-",
+    ?v => "...-",
+    ?w => ".--",
+    ?x => "-..-",
+    ?y => "-.--",
+    ?z => "--..",
+    ?1 => ".----",
+    ?2 => "..---",
+    ?3 => "...--",
+    ?4 => "....-",
+    ?5 => ".....",
+    ?6 => "-....",
+    ?7 => "--...",
+    ?8 => "---..",
+    ?9 => "----.",
+    ?0 => "-----",
+    ?\s => "..--"
+  }
+  def secret1(), do: '.- .-.. .-.. ..-- -.-- --- ..- .-. ..-- -... .- ... . ..-- .- .-. . ..-- -... . .-.. --- -. --. ..-- - --- ..-- ..- ...'
+  def secret2(), do: '.... - - .--. ... ---... .----- .----- .-- .-- .-- .-.-.- -.-- --- ..- - ..- -... . .-.-.- -.-. --- -- .----- .-- .- - -.-. .... ..--.. ...- .----. -.. .--.-- ..... .---- .-- ....- .-- ----. .--.-- ..... --... --. .--.-- ..... ---.. -.-. .--.-- ..... .----'
+  # The provided decoding tree
+  defp decode_table do
     {:node, :na,
      {:node, 116,
       {:node, 109,
@@ -23,96 +66,130 @@ defmodule Morse do
         {:node, 104, {:node, 52, nil, nil}, {:node, 53, nil, nil}}}}}}
   end
 
-  def encode_table, do: build_encode_table(morse_tree(), "")
+  @doc """
+  The time complexity of the encode function is O(n), where n is the length of the input text. This is because
+  each character in the input text is processed exactly once, and each processing step takes constant time on
+  average.
 
-  defp build_encode_table(nil, _), do: %{}
+  Specifically, the encode function performs the following steps:
 
-  defp build_encode_table({:node, :na, left, right}, current_code) do
-    Map.merge(
-      build_encode_table(left, current_code <> "."),
-      build_encode_table(right, current_code <> "-")
-    )
-  end
+    Convert the input text to lowercase using String.downcase(). This takes O(n) time, where n is the length of
+    the input text.
 
-  defp build_encode_table({:node, char, left, right}, current_code) do
-    Map.merge(
-      %{char => current_code},
-      Map.merge(
-        build_encode_table(left, current_code <> "."),
-        build_encode_table(right, current_code <> "-")
-      )
-    )
-  end
+    Convert the lowercase text to a charlist using String.to_charlist(). This takes O(n) time as well because
+    each character in the string needs to be converted to a Unicode codepoint in the charlist.
 
+    Call the encode_recursive function with the empty charlist as the initial value for acc. This takes constant
+    time.
+
+    encode_recursive processes each character in the charlist exactly once. For each character, it looks up its
+    Morse code in the map using Map.get(), which takes O(log k) time on average, where k is the number of keys
+    in the map (in this case, 36). Since k is a constant, we can treat this lookup as a constant-time operation
+    on average.
+
+    If the Morse code string is not nil, encode_recursive concatenates it to the acc charlist using the ++ operator.
+    If acc is empty, this takes O(m) time, where m is the length of the Morse code string. Otherwise, it takes
+    O(m + 1) time because the ++ operator first adds a space character to the acc charlist before concatenating
+    the Morse code string.
+
+    Finally, encode_recursive returns the resulting charlist as the encoded message.
+
+  Overall, the time complexity of the encode function is O(n) because each step takes constant time on average,
+  and the longest step (the charlist concatenation in step 5) takes O(m + 1) time, where m is a constant.
+  Therefore, the overall time complexity is linear in the length of the input text.
+  """
   def encode(text) do
-    table = encode_table()
-
     text
     |> String.downcase()
     |> String.to_charlist()
-    |> Enum.map(fn c -> Map.get(table, c, "") end)
-    |> Enum.join(" ")
-    |> String.trim()
+    |> encode_recursive([])
   end
 
+  defp encode_recursive([], acc), do: acc
 
-  def decode_table, do: build_decode_table(morse_tree(), "")
+  defp encode_recursive([char | rest], acc) do
+    case Map.get(@morse_codes, char) do
+      nil ->
+        encode_recursive(rest, acc)
 
-  defp build_decode_table(nil, _), do: %{}
-
-  defp build_decode_table({:node, :na, left, right}, current_code) do
-    Map.merge(
-      build_decode_table(left, current_code <> "."),
-      build_decode_table(right, current_code <> "-")
-    )
+      morse_code ->
+        case acc do
+          [] -> encode_recursive(rest, morse_code |> String.to_charlist())
+          _ -> encode_recursive(rest, (acc ++ [32] ++ morse_code) |> List.to_charlist())
+        end
+    end
   end
 
-  defp build_decode_table({:node, char, left, right}, current_code) do
-    Map.merge(
-      %{current_code => char},
-      Map.merge(
-        build_decode_table(left, current_code <> "."),
-        build_decode_table(right, current_code <> "-")
-      )
-    )
+  # Public function that decodes a Morse signal
+  # Input:
+  #   - morse_signal: a charlist containing the Morse signal to decode
+  # Output:
+  #   - the decoded message
+  def decode(morse_signal) do
+    # Build the decoding tree
+    tree = decode_table()
+    # Call the private decode function with the initial accumulator set to an empty list
+    decode(morse_signal, tree, [])
   end
 
-  def decode(encoded_text) do
-    table = decode_table()
+  @doc """
+  The time complexity of the decode function is O(n) where n is the length of the morse_signal string.
+  This is because the function calls decode_char on each element of the morse_signal string until it
+  is fully decoded or until an error occurs. Since each character is only visited once and the
+  decode_char function is O(1) (constant time), the time complexity of the decode function is O(n).
 
-    encoded_words = String.split(encoded_text, "  ")
+  The decode_char function is also O(1) because it is a simple pattern-matching operation that does
+  not depend on the length of the input. Therefore, the time complexity of the entire decoding
+  process is O(n).
+  """
+  # Private function that decodes a Morse signal using a decoding tree
+  # Input:
+  #   - morse_signal: charlist containing the Morse signal to decode
+  #   - tree: the decoding tree, built using the decode_table/0 function
+  #   - acc: the accumulator list, containing the decoded characters so far
+  # Output:
+  #   - list of decoded characters
+  defp decode([], _, acc), do: acc
 
-    decoded_words =
-      Enum.map(encoded_words, fn encoded_word ->
-        encoded_chars = String.split(encoded_word, " ")
-        decoded_chars = Enum.map(encoded_chars, &Map.get(table, &1, ""))
-        decoded_chars
-        |> Enum.map(&<<&1::utf8>>)
-        |> Enum.join("")
-      end)
+  defp decode(morse_signal, tree, acc) do
+    # Call the private decode_char function with the current Morse signal and the decoding tree
+    case decode_char(morse_signal, tree) do
+      # If the decode_char function returns :no, it means the Morse signal cannot be decoded
+      :no ->
+        :io.format("error: ~w\n", [Enum.take(morse_signal, 10)])
+        acc
 
-    Enum.join(decoded_words, " ")
+      # If the decode_char function returns a tuple {char, rest}, it means a character was successfully decoded
+      {char, rest} ->
+        # Call the decode function recursively with the remaining Morse signal and the updated accumulator
+        decode(rest, tree, acc ++ [char])
+    end
   end
 
-  defp decode_morse([], _table, _current_code, acc), do: Enum.join(acc)
+  # Private function that decodes the next character in a Morse signal using a decoding tree
+  # Input:
+  #   - morse_signal: binary containing the Morse signal to decode
+  #   - tree: the decoding tree, built using the decode_table/0 function
+  # Output:
+  #   - tuple {char, rest} if a character was successfully decoded
+  #   - :no if the Morse signal cannot be decoded
+  defp decode_char([], {_, char, _, _}), do: {char, []}
 
-defp decode_morse([char | rest], table, current_code, acc) do
-  case char do
-    ' ' ->
-      decode_morse(rest, table, "", acc)
-
-    _ ->
-      case Map.get(table, current_code <> <<char::utf8>>) do
-        nil ->
-          decode_morse(rest, table, current_code <> <<char::utf8>>, acc)
-
-        code when is_integer(code) ->
-          acc = acc ++ [<<code::utf8>>]
-          decode_morse(rest, table, "", acc)
-
-        _ ->
-          decode_morse(rest, table, current_code <> <<char::utf8>>, acc)
-      end
+  defp decode_char([?- | morse_signal], {_, _, long, _}) do
+    # If the next signal is a dash (-), follow the long path in the decoding tree
+    decode_char(morse_signal, long)
   end
-end
+
+  defp decode_char([?. | morse_signal], {_, _, _, short}) do
+    # If the next signal is a dot (.), follow the short path in the decoding tree
+    decode_char(morse_signal, short)
+  end
+
+  # If the Morse signal is empty or the decoding tree is nil, return :no
+  defp decode_char(_morse_signal, nil), do: :no
+  # If the next signal is a space and the decoding tree is in the "no match" state, return :no
+  defp decode_char([?\s | _morse_signal], {_, :na, _, _}), do: :no
+  # If the next signal is a space and the decoding tree is in a character state, return the
+  # decoded character and the remaining Morse signal
+  defp decode_char([?\s | morse_signal], {_, char, _, _}), do: {char, morse_signal}
 end
